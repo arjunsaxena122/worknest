8a07d74e7d1c548d2646d3687e981a5fdda3644b
arjunsaxena122 <arjunsaxena122@gmail.com>
2025-06-05 21:47:18 +0530

    ADD: make custom email sender and adding props in config

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -14,10 +14,14 @@ const createEnv = (processEnv) => {
     port: processEnv.PORT,
     mongouri: processEnv.MONGO_URI,
     node_env: processEnv.NODE_ENV,
-    access_token_key : processEnv.ACCESS_TOKEN_KEY,
-    access_token_expiry : processEnv.ACCESS_TOKEN_EXPIRY,
-    refresh_token_key : processEnv.REFRESH_TOKEN_KEY,
-    refresh_token_expiry : processEnv.REFRESH_TOKEN_EXPIRY
+    access_token_key: processEnv.ACCESS_TOKEN_KEY,
+    access_token_expiry: processEnv.ACCESS_TOKEN_EXPIRY,
+    refresh_token_key: processEnv.REFRESH_TOKEN_KEY,
+    refresh_token_expiry: processEnv.REFRESH_TOKEN_EXPIRY,
+    mailtrap_host: processEnv.MAILTRAP_HOST,
+    mailtrap_port: processEnv.MAILTRAP_PORT,
+    mailtrap_auth_user: processEnv.MAILTRAP_AUTH_USER,
+    mailtrap_auth_pass: processEnv.MAILTRAP_AUTH_PASS,
   };
 
   return processEnvObject;
--- server/src/utils/mail.js
+++ server/src/utils/mail.js
@@ -1,3 +1,58 @@
-const sendCustomMail = async() =>{
-    
-}
+import { env } from "../config/config.js";
+
+const sendCustomMail = async (options) => {
+  const mailGenerator = new Mailgen({
+    theme: "default",
+    product: {
+      name: "Task Manager",
+      link: "https://mailgen.js/",
+    },
+  });
+
+  const htmlEmail = mailGenerator.generate(options.mailGen);
+  const plainTextEmail = mailGenerator.generatePlainText(options.mailGen);
+
+  const transporter = nodemailer.createTransport({
+    host: env.mailtrap_host,
+    port: env.mailtrap_port,
+    secure: false,
+    auth: {
+      user: env.mailtrap_auth_user,
+      pass: env.mailtrap_auth_pass,
+    },
+  });
+
+  await transporter.sendMail({
+    from: options.fromSender,
+    to: options.toReceiver,
+    subject: options.subject,
+    text: plainTextEmail,
+    html: htmlEmail,
+  });
+};
+
+const emailVerificationCustomMail = (username, verificationUrl) => {
+  return {
+    body: {
+      name: username,
+      intro: "Welcome to my website! We're very excited to have you on board.",
+      action: {
+        instructions: "To get started with website, please click here:",
+        button: {
+          color: "#22BC66", // Optional action button color
+          text: "Verify your account",
+          link: verificationUrl,
+        },
+      },
+      outro:
+        "Need help, or have questions? Just reply to this email, we'd love to help.",
+    },
+  };
+};
+
+//* sendCustomMail({
+//*   mailGen: emailVerificationCustomMail("arjunsaxena", "http://localhost:3000"),
+//*   fromSender: "arjunsaxena122@gmail.com",
+// *  toReceiver: "deepak@gmail.com",
+// *  subject: "time pass time pass ....",
+//* });
