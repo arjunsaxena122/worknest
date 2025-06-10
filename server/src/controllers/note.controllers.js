96b9a168ef38fc07043d2cdb1f61a795dfa34e09
arjunsaxena122 <arjunsaxena122@gmail.com>
2025-06-10 09:46:47 +0530

    FIX: fixes the note routes and add new one that is getNoteById controller

--- server/src/controllers/note.controllers.js
+++ server/src/controllers/note.controllers.js
@@ -3,53 +3,56 @@ import { ApiError, ApiResponse, asyncHandler } from "../utils/index.js";
 
 const noteAdd = asyncHandler(async (req, res) => {
   const { content } = req.body;
-  const { nid } = req.params;
+  const { pid } = req.params;
 
   if (!content) {
     throw new ApiError(400, "Please fill the required field");
   }
 
-  //   const addNote = await ProjectNote.create({
-  //     content,
-  //     project: nid,
-  //     user: req?.user?.id,
-  //   });
+  const addNote = await ProjectNote.create({
+    content,
+    project: pid,
+    user: req?.user?.id,
+  });
 
-  const addNote = await ProjectNote.findOneAndUpdate(
-    { project: nid },
-    {
-      $set: {
-        content,
-      },
-      $setOnInsert: {
-        project: nid,
-        user: req?.user?.id,
-      },
-    },
-    {
-      new: true,
-      upsert: true,
-    },
-  );
+  const userNote = await ProjectNote.findById(addNote?._id).populate({
+    path: "user",
+    select: "username avatar",
+  });
 
-  if (!addNote) {
+  // const addNote = await ProjectNote.findOneAndUpdate(
+  //   { project: nid },
+  //   {
+  //     $set: {
+  //       content,
+  //     },
+  //     $setOnInsert: {
+  //       project: nid,
+  //       user: req?.user?.id,
+  //     },
+  //   },
+  //   {
+  //     new: true,
+  //     upsert: true,
+  //   },
+  // );
+
+  if (!userNote) {
     throw new ApiError(500, "Internal Server issue, Please retry to add note");
   }
 
   return res
     .status(201)
-    .json(new ApiResponse(201, "note add successfully", addNote));
+    .json(new ApiResponse(201, "note add successfully", userNote));
 });
 
 const noteDelete = asyncHandler(async (req, res) => {
   const { nid } = req.params;
 
-  const delNote = await ProjectNote.findOneAndDelete({
-    $and: [{ project: nid }, { user: req?.user?.id }],
-  });
+  const delNote = await ProjectNote.findByIdAndDelete(nid);
 
   if (!delNote) {
-    throw new ApiError(400, "Some issue arise with deleting note");
+    throw new ApiError(400, "Some issue arise with deleting the project note");
   }
 
   return res
@@ -62,7 +65,7 @@ const noteUpdate = asyncHandler(async (req, res) => {
   const { content } = req.body;
 
   const updateNote = await ProjectNote.findOneAndUpdate(
-    { project: nid },
+    nid,
     {
       $set: { content },
     },
@@ -80,17 +83,36 @@ const noteUpdate = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, "note update successfully", updateNote));
 });
 
-const noteGet = asyncHandler(async(req,res) =>{
+const getAllNote = asyncHandler(async (req, res) => {
+  const { pid } = req.params;
 
-    const {nid} = req.params
+  const getProjectNote = await ProjectNote.find({
+    $and: [{ project: pid }, { user: req?.user?.id }],
+  });
 
-    const getProjectNote = await ProjectNote.findOne({project:nid})
+  if (!getProjectNote) {
+    throw new ApiError(400, "note doesn't exist");
+  }
 
-    if(!getProjectNote){
-        throw new ApiError(400,"This note doesn't exist")
-    }
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, "Get the all note successfully", getProjectNote),
+    );
+});
 
-    return res.status(200).json(new ApiResponse(200,"Get the notes",getProjectNote))
-})
+const getNoteById = asyncHandler(async (req, res) => {
+  const { nid } = req.params;
 
-export { noteAdd, noteDelete, noteUpdate,noteGet };
+  const getUserNote = await ProjectNote.findById(nid);
+
+  if (!getUserNote) {
+    throw new ApiError(400, "note doesn't exist");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, "note found successfully", getUserNote));
+});
+
+export { noteAdd, noteDelete, noteUpdate, getAllNote, getNoteById };
--- server/src/routes/note.routes.js
+++ server/src/routes/note.routes.js
@@ -2,16 +2,18 @@ import { Router } from "express";
 import {
   noteAdd,
   noteDelete,
-  noteGet,
+  getAllNote,
   noteUpdate,
+  getNoteById,
 } from "../controllers/note.controllers.js";
 import { verifyAuthJwt } from "../middlewares/auth.middlewares.js";
 
 const router = Router();
 
-router.route("/create-note/:nid").post(verifyAuthJwt, noteAdd);
+router.route("/create-note/:pid").post(verifyAuthJwt, noteAdd);
 router.route("/delete-note/:nid").delete(verifyAuthJwt, noteDelete);
-// router.route("/update-note/:nid").put(verifyAuthJwt, noteUpdate);
-router.route("/get-note/:nid").get(verifyAuthJwt, noteGet);
+router.route("/update-note/:nid").put(verifyAuthJwt, noteUpdate);
+router.route("/get-all-note/:pid").get(verifyAuthJwt, getAllNote);
+router.route("/get-note/:nid").get(verifyAuthJwt, getNoteById);
 
 export default router;
