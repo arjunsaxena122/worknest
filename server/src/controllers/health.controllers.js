5311f1e0ece2ce411a902eb896e7caacf46c5ad4
arjunsaxena122 <arjunsaxena122@gmail.com>
2025-06-10 09:56:08 +0530

    CHANGE: health controllers response message

--- server/src/controllers/health.controllers.js
+++ server/src/controllers/health.controllers.js
@@ -4,10 +4,8 @@ export const healthCheck = asyncHandler((req, res) => {
   try {
     return res
       .status(200)
-      .json(
-        new ApiResponse(200, "Server running health is going on perfectly"),
-      );
+      .json(new ApiResponse(200, "Server runs state is healthy"));
   } catch (err) {
-    throw new ApiError(400, "Server running health is not good");
+    throw new ApiError(400, "Server runs state is not health");
   }
 });
